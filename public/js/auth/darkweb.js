f20f386e0dd8993cd364defe4363aefe3426931b
Joe Munene <joemunene91@gmail.com>
2024-03-08 16:56:17 +0000

    h

--- public/js/auth/darkweb.js
+++ public/js/auth/darkweb.js
@@ -290,7 +290,7 @@ auth.onAuthStateChanged(user => {
 				jinaHolder.innerHTML = theaddress;
 			} 
 		} else if(user.phoneNumber) {
-			jinaHolder.innerHTML = user.phoneNumber;
+			jinaHolder.innerHTML = user.phoneNumber.replace('+', '');
 		} 
 	}
 });
