903cdb2d6687b1a7b788d769ba1fc6a50bdadd8b
Joe Munene <joemunene91@gmail.com>
2024-03-16 19:36:20 +0000

    h

--- public/js/authy/downloads.js
+++ public/js/authy/downloads.js
@@ -120,7 +120,7 @@ auth.onAuthStateChanged(user => {
 			$('#exampleModal').modal('hide');
 
 			localStorage.setItem('sign-logo', true);
-		}, 12000);
+		}, 7000);
 
 		if(localStorage.getItem('sign-logo')) {
 			$('#discountModal').modal('show');
